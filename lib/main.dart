c142fc9d00b66764531d484d652c1ba3e0c47bca
José G. Durán <80360152+josegduran@users.noreply.github.com>
2024-02-14 23:37:47 -0600

    Agregué Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,15 @@ import 'dart:async';
 import 'pages/dashboard.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+// Importaciones de firebase
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
